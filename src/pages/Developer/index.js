7310ea89e05db78f5bc6a24548f2ecb08a1c36ae
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-12-15 13:35:07 +0100

    empty developer page created

---
+++ src/pages/Developer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+
+import styles from "./styles.module.css";
+
+export default function DeveloperPage() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <main></main>
+    </Layout>
+  );
+}
