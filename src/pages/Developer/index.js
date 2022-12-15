16f94bfed316a363503ada34a49e2f538d979eb0
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-12-15 14:11:05 +0100

    developer-content component called at src/pages/Developer for display

--- src/pages/Developer/index.js
+++ src/pages/Developer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import DeveloperHeader from "../../components/DeveloperHeader";
+import DeveloperContent from "../../components/DeveloperContent";
 
 import styles from "./styles.module.css";
 
@@ -14,7 +15,9 @@ export default function DeveloperPage() {
       description="Description will go into a meta tag in <head />"
     >
       <DeveloperHeader />
-      <main></main>
+      <main>
+        <DeveloperContent />
+      </main>
     </Layout>
   );
 }
