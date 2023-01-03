2328e07211217efef960541aa6150299462ad361
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-01-03 18:06:24 +0100

    compressing developer page content files one more time

--- src/components/DeveloperContent/index.js
+++ src/components/DeveloperContent/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-import EcosystemImg from "@site/static/img/ecosystem-min.png";
-import ThreeViewsImg from "@site/static/img/three_views-min.png";
+import EcosystemImg from "@site/static/img/ecosystem-minified.png";
+import ThreeViewsImg from "@site/static/img/three_views-minified.png";
 
 export default function DeveloperContent() {
   return (
