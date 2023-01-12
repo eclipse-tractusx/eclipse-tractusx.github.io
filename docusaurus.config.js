1ef457668a73a97427b9725dafafbfd4807679ac
Siegfried Kiermayer <siegfried.kiermayer@sap.com>
2023-01-12 14:05:00 +0100

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/eclipse-tractusx/eclipse-tractusx.github.io/tree/main/docs/',
+            'https://github.com/eclipse-tractusx/eclipse-tractusx.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/eclipse-tractusx/eclipse-tractusx.github.io/tree/main/docs/',
+            'https://github.com/eclipse-tractusx/eclipse-tractusx.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
