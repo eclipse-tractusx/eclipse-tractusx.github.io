be8056c21b09887616881705dff3109d3f06a139
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-01-12 09:02:40 +0100

    Adoption view tab from business kit page defined, need to import img files

---
+++ src/pages/BusinessKit/AdoptionView/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+
+import KitsHeader from "@site/src/components/KitsHeader";
+import KitsCard from "@site/src/components/KitsCard";
+import Adoption from "@site/docs/kits/Business-Kit/_adoption-view.md";
+
+import KitBusinessSvg from '@site/static/img/kit_business_logo.svg'
+import Benefits from "@site/static/img/benefits.png"
+import BPDM from "@site/static/img/bpdm-card.png";
+
+import styles from "./styles.module.css";
+
+
+export default function AdoptionView() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={'Adoption View'}
+      description="Description will go into a meta tag in <head />"
+    >
+      <KitsHeader 
+        title="Business Partner Kit" 
+        subtitle="Use BusinessParterKit in your app or service to get a high-quality data record including a unique identifier of business partners or to connect as a sharing member."
+        SVG={KitBusinessSvg}
+      />
+      <main>
+        <div className={styles.info_container}>
+          <div className={styles.paragraph}>
+            <Adoption />
+          </div>
+          <div className={styles.benefits}>
+            <h2 className={styles.title}>Benefits</h2>
+            <p className={styles.subtitle}>The three main benefits of using the BP KIT (Business Partner Kit).</p>
+            <img src={Benefits} className={styles.img}/>
+          </div>
+        </div>
+        <KitsCard 
+          title="Explore the business partner data managment."
+          subtitle="Especially the  Golden Records for business partners and the Value Added Services (VAS)."
+          img={BPDM}
+        />
+      </main>
+    </Layout>
+  );
+}
