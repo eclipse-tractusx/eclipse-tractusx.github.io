b4c4e29212cc5e393cfaf4ff0c0e621a55ed7c74
Daniel Miehle <daniel.miehle@bmw.de>
2023-01-25 23:18:41 +0100

    Update index.js

--- src/components/AboutUsContent/index.js
+++ src/components/AboutUsContent/index.js
@@ -37,7 +37,7 @@ export default function AboutUsContent() {
 
         <div className={styles.information_container}>
           <p className={styles.description}>
-           The Eclipse Tractus-X™ project is the official open-source project in the Catena-X ecosystem under the umbrella of the Eclipse Foundation. The Eclipse Foundation is not-for-profit corporation that it supported by over 320 members, and represents the worlds largest sponsored collection of Open Source projects and developers. It provides our global community of individuals and organizations with a mature, scalable, and business-friendly environment for open source software collaboration and innovation.
+           The Eclipse Tractus-X™ project is the official open-source project in the Catena-X ecosystem under the umbrella of the Eclipse Foundation. The Eclipse Foundation is not-for-profit corporation that it supported by over 320 members, and represents the worlds largest sponsored collection of Open Source projects and developers.
           </p>
         </div>
 
@@ -63,16 +63,16 @@ export default function AboutUsContent() {
           (3) In the <strong>operating environment</strong>, the open-source reference implementations can be freely used, modified, and operated by providers - including core service provider (e.g., marketplace), enablement service provider (e.g., Eclipse Dataspace Connector), and business application provider (e.g., traceability applications). 
           </p>
 
-          <p className={styles.description}>
-          Tractus-X supports developers to accelerate the development and operation of services / applications and to contribute significantly to the rapid scaling of the ecosystem. Therefore Tractus-X provides reference implementations of core and enabling services as well as KITs to accelerate the development of interoperable and innovative applications and the on-boarding to the Catena-X ecosystem. 
-          </p>
-
         </div>
 
         <div className={styles.information_container}>
           
+           <p className={styles.description}>
+          Tractus-X supports developers to accelerate the development and operation of services / applications and to contribute significantly to the rapid scaling of the ecosystem. Therefore Tractus-X provides reference implementations of core and enabling services as well as KITs to accelerate the development of interoperable and innovative applications and the on-boarding to the Catena-X ecosystem. 
+          </p>
+          
           <p className={styles.description}>
-          Reference implementations are free and open-source software (FOSS) components, which are managed in the associated Eclipse Tractus-X project. All information about the reference implementations (e.g., project structure, development, and release processes) can be found in the future on the Eclipse Tractus-X project website and will follow the Eclipse Foundation Development Process. The repositories of Eclipse Tractus-X contain, among other things, source code, documentation, and deployment instructions. Other reference implementations (e.g., DAPS) used by the Catena-X ecosystem but developed by other projects such as Gaia-X or IDSA can also be found in the Eclipse Tractus-X project. For a complete list of reference implementations used, see the Catena-X Association website.
+          Reference implementations are free and open-source software (FOSS) components, which are managed in the associated Eclipse Tractus-X project. Other reference implementations (e.g., DAPS) used by the Catena-X ecosystem but developed by other projects such as Gaia-X or IDSA can also be found in the Eclipse Tractus-X project.
           </p>
 
           <p className={styles.description}>
@@ -80,19 +80,23 @@ export default function AboutUsContent() {
           </p>
         </div>
 
-        <div className={styles.img_container}>
-          <img className={styles.licenses} src={LicensesImg} /> 
-        </div>
-
         <div className={styles.title_container}>
-          <h2 className={styles.title}>Tractus-X KITs</h2>
+          <h2 className={styles.title}>KITs</h2>
         </div>
   
         <div className={styles.information_container}>
           <p className={styles.description}>
-          Application and services providers can utilize the KITs to create an interoperable, innovative, and scalable multi-vendor solution portfolio. Data provider and consumer can utilize KITs to boost adoption and easy deployment. KITs include jointly developed standards and artifacts to foster trust and collaboration governed by the Catena-X association. KITs can be used across domains and are open for use of any application, service and even other KITs. KITs are breeding ground for innovation, efficient development, and interoperability.
+          Application and service providers can utilize the KITs to create an interoperable, innovative, and scalable multi-vendor solution portfolio. Data provider and consumer can utilize KITs to boost adoption and easy deployment.
+          </p>
+
+          <p className={styles.description}>
+          KITs include jointly developed standards and artifacts to foster trust and collaboration governed by the Catena-X association. KITs can be used across domains and are open for use of any application, service and even other KITs. KITs are breeding ground for innovation, efficient development, and interoperability.
           </p>
          </div>
+
+        <div className={styles.img_container}>
+          <img className={styles.licenses} src={LicensesImg} /> 
+        </div>
         
       </div>
     </section>
