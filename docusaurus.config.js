6c4570680727fe195fc8a68b397c9e4f015fa1a8
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-01-26 11:34:40 +0100

    community page created under ./src/pages folder and navigation item added to navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,6 +130,11 @@ const config = {
             position: 'left',
             label: 'Developer Hub',
           },
+          {
+            to: "/community",
+            position: "left",
+            label: "Community",
+          },
           /* {to: '/blog', label: 'Blog', position: 'left'}, */
           {
             href: 'https://github.com/eclipse-tractusx/eclipse-tractusx.github.io',
