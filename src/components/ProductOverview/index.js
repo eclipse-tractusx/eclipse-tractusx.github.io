c854320e6e01ed31f1b257e6c03aba43259a6022
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-02-13 10:39:17 +0100

    products array defined in utils/products.js file and imported in the ProductOverview component

--- src/components/ProductOverview/index.js
+++ src/components/ProductOverview/index.js
@@ -21,27 +21,11 @@
 
 import React from "react";
 import ProductOverviewCard from "../ProductOverviewCard";
+import { products } from "../../../utils/products";
 
 import styles from "./styles.module.css";
  
 export default function ProductOverview() {
-  const products = [
-    {
-      productName: "Portal & Marketplaces",
-      productDescription: "The Portal & Marketplaces product offers the entry point into the Catena-X data room for registration, (technical) onboarding and beyond that for all participants. In addition, marketplaces enable solution providers to offer various solutions (e.g., business applications, services). to end customers.", 
-      githubRepo: [
-        "https://github.com/eclipse-tractusx/portal-backend",
-        "https://github.com/eclipse-tractusx/portal-frontend"
-      ], 
-      committers: [
-        "https://github.com/evegufy", 
-        "https://github.com/ntruchsess",
-        "https://github.com/oyo"
-      ],
-      mailTo: "tractusx-dev@eclipse.org?subject=Request Portal and Marketplace Team"
-    },
-  ]
-
   return (
     <section className={styles.product_overview}>
       <div className={styles.container}>
