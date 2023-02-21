1fbf1f3dd27393dc891cdf6b17f313d3f5ae8348
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-02-21 12:30:08 +0100

    gallery images replaced with new content & kits array moved out from KitsGallery component and declared in utils/galleryKits.js

---
+++ utils/galleryKits.js
@@ -0,0 +1,26 @@
+import Business_Kit from "@site/static/img/gallery-business-partner-kit-minified.png";
+import DataChain_Kit from "@site/static/img/gallery-data-chain-kit-minified.png";
+import Connector_Kit from "@site/static/img/gallery-conector-kit-minified.png";
+import MoreToCome from "@site/static/img/gallery-more_coming-minified.png";
+
+export const galleryKits = [
+  {
+    id: 1,
+    img: Business_Kit,
+    pageRoute: "docs/kits/Business Partner Kit/Adoption View"
+  },
+  {
+    id: 2,
+    img: DataChain_Kit,
+    pageRoute: "docs/kits/Data Chain Kit/Adoption View"
+  },
+  {
+    id: 3,
+    img: Connector_Kit,
+    // pageRoute: "docs/kits/Data Chain Kit/Adoption View"
+  },
+  {
+    id: 4,
+    img: MoreToCome
+  },
+]
