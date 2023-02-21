03dc5630d81d302865ff4e346c1bc100e6b1cc5a
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-02-21 12:36:42 +0100

    fake testing data removed from utils/newsTitle.js

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -4,14 +4,4 @@ export const newsTitles = [
     title: "Eclipse Tractus-X Developer Portal is LIVE!",
     blogLink: "/blog/portal-is-live"
   },
-  {
-    date: "00.00.0000",
-    title: "TESTING SECOND ITEM",
-    blogLink: "/blog/portal-is-live"
-  },
-  {
-    date: "01.01.0001",
-    title: "TESTING THE THIRD ITEM",
-    blogLink: "/blog/portal-is-live"
-  },
 ]
