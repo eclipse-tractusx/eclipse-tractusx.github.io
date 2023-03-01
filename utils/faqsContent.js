b9a4c38002b98760f727cdbae18f781579fbebd5
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-03-01 11:45:32 +0100

    utils/faqsContent file imported in FAQsComponent

---
+++ utils/faqsContent.js
@@ -0,0 +1,14 @@
+export const faqsContent = [
+  {
+    question: "Why are there KITs and where does this naming comes from?",
+    answer: "KIT means “Keep It Together” - Catena-X KITs aim to accelerate the development of Catena-X applications and services and contribute significantly to the rapid scaling of the Catena-X ecosystem."
+  },
+  {
+    question: "Are there more KITs coming?",
+    answer: "There are the first three lighthouse KITs available. More and more KITs will come from different domains."
+  },
+  {
+    question: "Does every KIT contain the same things and follow the same structure?",
+    answer: "KITs are guided with three different views (adoption, develop and operation) but not every KIT will provide the same objectives. Some will be more focused on the adoption view with a vision & mission, semantic model .. and other KITs will have a different target-group like developers - so there will be more focus on the technical specification."
+  }
+]
