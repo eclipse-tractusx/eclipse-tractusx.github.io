9389e5e419e66fba8a9e48921a540245d10d9fe1
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-03-01 11:45:32 +0100

    utils/faqsContent file imported in FAQsComponent

--- src/components/FAQsComponent/index.js
+++ src/components/FAQsComponent/index.js
@@ -26,6 +26,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { faqsContent } from "@site/utils/faqsContent";
 
 import styles from "./styles.module.css";
 
