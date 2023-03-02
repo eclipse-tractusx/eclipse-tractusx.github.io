42e72ab05feea935877d13ff923fcdf9afc1eed5
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-03-02 17:53:34 +0100

    carousel component refactored to manage its content from utils/carouselKits where each item is defined

---
+++ utils/carouselKits.js
@@ -0,0 +1,36 @@
+import Business_Kit from "@site/static/img/business_kit-minified.png";
+import Conector_Kit from "@site/static/img/conector_kit-minified.png";
+import DataChain_Kit from "@site/static/img/datachain_kit-minified.png";
+
+export const carouselKits = [
+  {
+    id: 1,
+    img: Business_Kit,
+    pageRoute: "docs/kits/Business Partner Kit/Adoption View"
+  },
+  {
+    id: 2,
+    img: Conector_Kit,
+    pageRoute: "docs/kits/product-edc/docs/kit/adoption-view/Adoption%20View"
+  },
+  {
+    id: 3,
+    img: DataChain_Kit,
+    pageRoute: "docs/kits/Data Chain Kit/Adoption View"
+  },
+  {
+    id: 4,
+    img: Business_Kit,
+    pageRoute: "docs/kits/Business Partner Kit/Adoption View"
+  },
+  {
+    id: 5,
+    img: Conector_Kit,
+    pageRoute: "docs/kits/product-edc/docs/kit/adoption-view/Adoption%20View"
+  },
+  {
+    id: 6,
+    img: DataChain_Kit,
+    pageRoute: "docs/kits/Data Chain Kit/Adoption View"
+  },
+]
