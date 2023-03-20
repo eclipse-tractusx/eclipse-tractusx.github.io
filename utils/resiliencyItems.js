28d3726f7f919bd30831c168ef2179310c7bb402
Maximilian Ong <ong@mercedes-benz.com>
2023-03-20 10:29:50 +0100

    fixing conflicts

--- utils/resiliencyItems.js
+++ utils/resiliencyItems.js
@@ -2,7 +2,7 @@ import Business_Kit from "@site/static/img/gallery-business-partner-kit-minified
 import DataChain_Kit from "@site/static/img/gallery-data-chain-kit-minified.png";
 import Connector_Kit from "@site/static/img/gallery-conector-kit-minified.png";
 import MoreToCome from "@site/static/img/gallery-more_coming-minified.png";
-import Maas_Kit from "@site/static/img/MaaSKit.png";
+import Maas_Kit from "@site/static/img/MaaSKIT.png";
 import OSim_Kit from "@site/static/img/OSimKIT.png";
 import PURIS_Kit from "@site/static/img/PURISKIT.png";
 import MP_Kit from "@site/static/img/MPKIT.png";
