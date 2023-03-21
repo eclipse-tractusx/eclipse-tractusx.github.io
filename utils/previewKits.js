72ae820f732be988bcf6cdee743cb35e090bc3b5
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-03-21 11:07:28 +0100

    KitsGallery component styles adjusted and reuse in developer page, new utils/previewKits.js file defined to manage content

---
+++ utils/previewKits.js
@@ -0,0 +1,38 @@
+import DCM_Kit from "@site/static/img/gallery-dcm-kit-min.png";
+import Maas_Kit from "@site/static/img/MaaSKIT.png";
+import OSim_Kit from "@site/static/img/OSimKIT.png";
+import PURIS_Kit from "@site/static/img/PURISKIT.png";
+import MP_Kit from "@site/static/img/MPKIT.png";
+import MoreToCome from "@site/static/img/gallery-more_coming-minified.png";
+
+export const previewKits = [
+	{ 
+		id: 1,
+		img: DCM_Kit,
+		pageRoute: "/docs/Resiliency/DCM%20Kit"
+	},
+  { 
+		id: 2,
+		img: Maas_Kit,
+		pageRoute: "/docs/Resiliency/maas"
+	},
+	{ 
+		id: 3,
+		img: OSim_Kit,
+		pageRoute: "/docs/Resiliency/OSIM"
+	},
+	{ 
+		id: 4,
+		img: PURIS_Kit,
+		pageRoute: "/docs/Resiliency/PURIS"
+	},
+	{ 
+		id: 5,
+		img: MP_Kit,
+		pageRoute: "/docs/Resiliency/mp%20kit"
+	},
+  {
+    id: 4,
+    img: MoreToCome
+  },
+]
