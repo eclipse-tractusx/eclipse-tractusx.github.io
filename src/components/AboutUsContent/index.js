47d8b007303a1cd0f2cd6b87d4a7f4d47750fdae
santiago julian monsalve <s.julian.monsalve@accenture.com>
2023-05-09 15:18:52 +0200

    margin and padding adjusted accross components

--- src/components/AboutUsContent/index.js
+++ src/components/AboutUsContent/index.js
@@ -32,19 +32,14 @@ export default function AboutUsContent() {
 
         <div className={styles.title_container}>
           <h2 className="title">Background</h2>
-        </div>
 
-        <div className={styles.information_container}>
           <p className="description">
            The Eclipse Tractus-X™ project is the official open-source project in the Catena-X ecosystem under the umbrella of the Eclipse Foundation. The Eclipse Foundation is not-for-profit corporation that it supported by over 320 members, and represents the worlds largest sponsored collection of Open Source projects and developers.
           </p>
-        </div>
 
-        <div className={styles.img_container}>
-          <TractusXBG className={styles.img}/>
-        </div>
-
-        <div className={styles.information_container}>
+          <div className={styles.img_container}>
+            <TractusXBG className={styles.img}/>
+          </div>
 
           <p className="description">
           The Catena-X ecosystem consists of the following three areas, which are connected by the Tractus-X project:
@@ -61,10 +56,6 @@ export default function AboutUsContent() {
           <p className="description">
           (3) In the <strong>operating environment</strong>, the open-source reference implementations can be freely used, modified, and operated by providers - including core service provider (e.g., marketplace), enablement service provider (e.g., Eclipse Dataspace Connector), and business application provider (e.g., traceability applications). 
           </p>
-
-        </div>
-
-        <div className={styles.information_container}>
           
           <p className="description">
           Tractus-X supports developers to accelerate the development and operation of services / applications and to contribute significantly to the rapid scaling of the ecosystem. Therefore Tractus-X provides reference implementations of core and enabling services as well as KITs to accelerate the development of interoperable and innovative applications and the on-boarding to the Catena-X ecosystem. 
