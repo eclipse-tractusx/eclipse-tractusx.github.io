f8eb946cc8485dd3be5325e3c1c0502001dec06b
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-09-08 13:45:25 +0530

    udpates(news gallery): remove 3.1 release text from news gallery

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -5,11 +5,6 @@ export const newsTitles = [
     blogLink: "/blog/portal-is-live"
   },
   {
-    date: "26.05.2023",
-    title: "Tractus-X 3.1 is available now",
-    blogLink: "/blog/new-release-3-1-0"
-  },
-  {
     date: "08.09.2023",
     title: "Tractus-X 23.09 is available now",
     blogLink: "/blog/new-release-23.09"
