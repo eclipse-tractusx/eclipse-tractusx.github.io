41eeb47b967cb3f35d14751201832b663a9c36a7
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-10-17 17:54:52 +0530

    updates: hide role based entry courosel. Update icon and content for community contribute section. Remove Resiliency from 23.09 version. Add new link in header preview

--- utils/carouselCommunityInfo.js
+++ utils/carouselCommunityInfo.js
@@ -1,55 +1,39 @@
 import codeIcon from "@site/static/img/code-icon.png";
 import contributeIcon from "@site/static/img/contribute-icon.png"
+import faqIcon from "@site/static/img/faq.png";
+import communityIcon from "@site/static/img/community.png"
 
 export const carouselCommunityInfo = [
     {
         id: 0,
         title: "Community.",
         subtitle: "Contribute.",
-        description: "Lorem ipsum dolor sit amet consectetur. Cursus hac adipiscing ac aenean pellentesque. Arcu sed urna quis cursus nec eu ut fusce morbi est cum lauda.",
+        description: "Get familiar with the guidelines and governance you’ll need to contribute to this project. The Tractus-X community includes 8 products that are available.",
         textCard: true
     },
     {
         id: 1,
         title: "DevHub",
-        description: "Lorem Ipsum",
-        navigate: "/",
+        navigate: "/docs/developer",
         icon: codeIcon
     },
     {
         id: 2,
         title: "Community",
-        description: "",
-        navigate: "/",
-        icon: codeIcon
+        navigate: "/community",
+        icon: communityIcon
     },
     {
         id: 3,
         title: "Contribute a KIT",
-        description: "",
-        navigate: "/",
+        navigate: "/docs/kit-process/graduation-process",
         icon: contributeIcon
     },
     {
         id: 4,
         title: "FAQ",
-        description: "",
-        navigate: "/",
-        icon: codeIcon
-    },
-    {
-        id: 5,
-        title: "Contribute a KIT",
-        description: "",
-        navigate: "/",
-        icon: contributeIcon
-    },
-    {
-        id: 6,
-        title: "FAQ",
-        description: "",
-        navigate: "/",
-        icon: codeIcon
-    },
+        navigate: "/docs/kit-process/processes/kit-faq",
+        icon: faqIcon
+    }
   ]
   
