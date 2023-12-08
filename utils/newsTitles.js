6de8181cc1f89ca67dc4e1ebaa7ba84526c21458
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-12-08 18:28:49 +0530

    updates: revert blog related changes

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -1,10 +1,5 @@
 export const newsTitles = [
   {
-    date: "08.12.2023",
-    title: "Tractus-X 23.12 is available now",
-    blogLink: "/blog/new-release-2312"
-  },
-  {
     date: "28.11.2023",
     title: "New launch of tutorials section / First E2E adopter Journey tutorial",
     blogLink: "/blog/release-tutorials"
@@ -28,5 +23,5 @@ export const newsTitles = [
     date: "08.09.2023",
     title: "Tractus-X 23.09 is available now",
     blogLink: "/blog/new-release-23.09"
-  }
+  },
 ]
