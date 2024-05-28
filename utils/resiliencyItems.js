540008efe5fb7b96b31ea6f08317f652f0a91a89
Johann Schütz <johann@schuetz.pro>
2024-05-28 17:45:48 +0200

    Update "Upcoming KITs"

--- utils/resiliencyItems.js
+++ utils/resiliencyItems.js
@@ -8,26 +8,26 @@ export const resiliencyItems = [
 	{
 		id: 1,
 		img: DCM_Kit,
-		pageRoute: "/docs-kits/kits/Resiliency/DCM%20Kit"
+		pageRoute: "/docs-kits/category/dcm-kit"
 	},
   {
 		id: 2,
 		img: Maas_Kit,
-		pageRoute: "/docs-kits/kits/Resiliency/maas"
+		pageRoute: "/docs-kits/category/manufacturing-as-a-service-kit"
 	},
 	{
 		id: 3,
 		img: OSim_Kit,
-		pageRoute: "/docs-kits/next/kits/OSim%20Kit/Adoption%20View%20OSim%20Kit"
+		pageRoute: "/docs-kits/category/osim-kit"
 	},
 	{
 		id: 4,
 		img: PURIS_Kit,
-		pageRoute: "/docs-kits/next/kits/PURIS%20Kit/Adoption%20View%20PURIS%20Kit"
+		pageRoute: "/docs-kits/category/puris-kit"
 	},
 	{
 		id: 5,
 		img: MP_Kit,
-		pageRoute: "/docs-kits/kits/Resiliency/mp%20kit"
+		pageRoute: "/docs-kits/category/modular-production-kit"
 	},
 ]
