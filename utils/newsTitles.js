fb28957e33fe6dd2f694eddd1604ac021e95d910
Stephan Bauer <stephan.bauer@catena-x.net>
2024-10-09 09:26:46 +0200

    add new blog entry for refinement day 1 in R25.03

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -1,5 +1,10 @@
 export const newsTitles = [
   {
+    date: "16.10.2024",
+    title: "Refinement Day 1 for Release 25.03",
+    blogLink: "/blog/refinement-day-1-R24.03"
+  },
+  {
     date: "05.12.2024",
     title: "Third Eclipse Tractus-X Community Days",
     blogLink: "/blog/community-days-12-2024"
@@ -8,20 +13,5 @@ export const newsTitles = [
     date: "28.05.2024",
     title: "Tractus-X 24.05 is available now",
     blogLink: "/blog/new-release-2405"
-  },  
-  {
-    date: "31.07.2024",
-    title: "Tractus-X Open Planning R24.12",
-    blogLink: "/blog/open-planning-r24-12"
   },
-  {
-    date: "08.03.2024",
-    title: "Tractus-X 24.03 available now",
-    blogLink: "/blog/new-release-2403"
-  },
-  {
-    date: "19.02.2024",
-    title: "Tractus-X Open Planning R24.08",
-    blogLink: "/blog/open-planning-r24-08"
-  }
 ]
