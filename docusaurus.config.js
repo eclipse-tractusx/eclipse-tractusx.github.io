8f2dfdeaa71f69aa6de932cdfa187d1036139ec0
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-03-31 14:00:44 +0200

    make IDs more consistent, enabled nav- und side-bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/kits/business-partner-kit/adoption-view',
+                label: 'Business Partner',
+              },
             ],
           },
           {
