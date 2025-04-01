02d5ed4dbfe4c58b329e7525001ab3850ffb5b04
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-04-01 12:14:40 +0200

    Enabled Nav and Sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/kits/knowledge-agents/adoption-view/intro',
+                label: 'Knowledge Agents',
+              },
             ],
           },
           {
