ea7e50c96e0f4353c92a60915b7d37429fdd5cc3
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-04-02 10:20:24 +0200

    Reenabled Nav- and Sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/kits/environmental-and-social-standards-kit/adoption-view',
+                label: 'Environmental and Social Standards',
+              },
             ],
           },
           {
