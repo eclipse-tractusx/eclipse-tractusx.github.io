86416bc28d74565ca714c282c1d44a27c474cfa9
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-04-09 10:57:52 +0200

    Setup Nav and Sidebar --> Integrated API Hub Link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/kits/data-chain-kit/adoption-view',
+                label: 'Data Chain',
+              },
             ],
           },
           {
