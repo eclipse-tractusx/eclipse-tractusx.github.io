53131a7d2ae126065a86415b56fb0bd59b8d2b98
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-04-15 10:46:07 +0200

    navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/kits/circularity-kit/adoption-view',
+                label: 'Circularity',
+              },
             ],
           },
           {
