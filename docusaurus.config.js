82b725a4478098adef6b5e70043bcf76866fedd0
Philipp <35833350+ClosedSourcerer@users.noreply.github.com>
2025-04-17 11:02:25 +0200

    Gallery and NavBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 to: '/docs-kits/dummy-kit/adoption-view',
                 label: 'Dummy Kit',
               },
+              {
+                to: '/docs-kits/behaviour-twin-kit/overview',
+                label: 'Behaviour Twin',
+              },
             ],
           },
           {
