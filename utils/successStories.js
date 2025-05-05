81f8fa5aeb70d6c9bc762dbea9a11435d5618264
Stephan Bauer <stephan.bauer@catena-x.net>
2025-05-05 13:30:37 +0200

    first set of broken link fixes

--- utils/successStories.js
+++ utils/successStories.js
@@ -7,15 +7,15 @@ export const successStories = [
     img: tracex,
     title: "TRACE-X SUCESS STORY",
     description: "Trace-X empowers all companies from SMEs to large OEMs to participate in parts traceability with an Open-Source solution.",
-    pageRoute: "/docs-kits/kits/data-chain-kit/Success Stories/Trace-X Sucess Story",
-    buttonName: "SUCESS STORY"
+    pageRoute: "/docs-kits/kits/data-chain-kit/adoption-view",
+    buttonName: "SUCCESS STORY"
   },
   {
     id: 2,
     img: countryRisk,
-    title: "COUNTRY RISK SUCESS STORY",
+    title: "COUNTRY RISK SUCCESS STORY",
     description: "The service offers a risk score per country based on several, customizable ratings.",
-    pageRoute: "/docs-kits/kits/business-partner-kit/Success Stories/Country Risk Score%C2%A0",
+    pageRoute: "/docs-kits/kits/business-partner-kit/success-stories/country-risk",
     buttonName: "SUCESS STORY"
   }
 ]
