efc3536c560a5b48acc4f084c1729a257303af61
Mathias Moser <mathias.moser@catena-x.net>
2025-08-04 19:01:33 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'eclipse-tractusx', // Usually your GitHub org/user name.
-  projectName: 'eclipse-tractusx.github.io', // Usually your repo name.
+  organizationName: 'matbmoser', // Usually your GitHub org/user name.
+  projectName: 'next-eclipse-tractusx.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
