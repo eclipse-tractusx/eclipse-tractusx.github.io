58d9613dca587c2bf7ea523a7d8c919e1b0b4f1d
Stephan Bauer <stephan.bauer@catena-x.net>
2025-10-09 13:52:54 +0200

    Adds blog post for fifth community days

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -1,5 +1,10 @@
 export const newsTitles = [
     {
+        date: "04.12.2025",
+        title: "Fifth Eclipse Tractus-X Community Days",
+        blogLink: "/blog/community-days-12-2025"
+    },
+    {
         date: "01.07.2025",
         title: "Tractus-X Community Update - Release Planning for Release 25.12",
         blogLink: "/blog/release-planning-R25.12"
