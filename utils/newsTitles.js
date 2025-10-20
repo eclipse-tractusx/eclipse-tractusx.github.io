ddd4e2d0867302a05c3d661df988a27ce583ba2e
Stephan Bauer <stephan.bauer@catena-x.net>
2025-10-20 10:12:30 +0200

    Adds test phase R25.12 meeting information

--- utils/newsTitles.js
+++ utils/newsTitles.js
@@ -1,5 +1,10 @@
 export const newsTitles = [
   {
+    date: "20.10.2025",
+    title: "Test Phase for Release 25.12",
+    blogLink: "/blog/testphase-R25.12"
+  },
+  {
     date: "30.10.2025",
     title: "Tractus-X Community Update - Alignment Day for Release 26.03",
     blogLink: "/blog/release-planning-R26.03"
@@ -13,10 +18,5 @@ export const newsTitles = [
     date: "04.12.2025",
     title: "Fifth Eclipse Tractus-X Community Days",
     blogLink: "/blog/community-days-12-2025"
-  },
-  {
-    date: "01.07.2025",
-    title: "Tractus-X Community Update - Release Planning for Release 25.12",
-    blogLink: "/blog/release-planning-R25.12"
   },
 ]
