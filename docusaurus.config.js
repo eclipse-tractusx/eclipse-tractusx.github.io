f62b0036264d10ea718ac090d57c864f1816214f
Stephan Bauer <stephan.bauer@catena-x.net>
2025-11-04 20:51:27 +0100

    add new info banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -253,7 +253,7 @@ const config = {
       },
       announcementBar: {
         id: `announcementBar-v25.09`,
-        content: `🎉️ <b><a href="/blog-changelog/release-25-09">Eclipse Tractus-X 25.09</a> is out!</b> 🥳️`,
+        content: `🎉️ New <b><a href="/docs/oss/release-process">Eclipse Tractus-X Release Documentation</a></b> and new <b><a href="/community/open-meetings">Open Meetings Calendar</a></b> 🥳️`,
       },
       navbar: {
         title: 'Eclipse Tractus-X',
