fd160672367a9c6bc19243c9d4e4c0c1badf46f2
Mathias Moser <mathias.moser@catena-x.net>
2025-11-20 19:56:42 +0100

    feat: refactor KitsArchitecture component to utilize industry data; remove KitsExtensions component and styles

--- src/components/2.0/KitsArchitecture/index.js
+++ src/components/2.0/KitsArchitecture/index.js
@@ -20,13 +20,13 @@
  import Link from "@docusaurus/Link";
  import styles from "./styles.module.scss";
  
- export default function KitsArchitecture({dataspaces}) {
+ export default function KitsArchitecture({industries}) {
    return (
      <section className={styles.architecture_section}>
        <div className={`${styles.container}`}>
          <h2 className={`${styles.title}`}>Our KITs Architecture</h2>
          
-         <p className={styles.subtitle}>Building the foundation for interoperable dataspaces across industries</p>
+         <p className={styles.subtitle}>Building the foundation for interoperable solutions across industries</p>
          
          {/* Interactive hint */}
          <div className={styles.interactive_hint}>
@@ -39,37 +39,38 @@
          <div className={styles.diagram}>
            {/* Top row hexagons */}
            <div className={styles.hexagon_row}>
-             {dataspaces.map((dataspace) => (
-               <Link 
-                 key={dataspace.id}
-                 to={`/Kits/dataspace?id=${dataspace.id}&ref=architecture`}
-                 className={`${styles.hexagon} ${styles[`hexagon_${dataspace.id.replace('-', '_')}`]}`}
-                 style={{
-                   '--hexagon-gradient': dataspace.gradient,
-                   '--hexagon-title-gradient': dataspace.gradient.replace('135deg', '45deg'),
-                 }}
-               >
-                 <div className={styles.hexagon_content}>
-                   <div className={styles.hexagon_title}>
-                     <img 
-                       src={dataspace.logo.src} 
-                       alt={dataspace.logo.alt}
-                       width={dataspace.logo.width}
-                       height={dataspace.logo.height === 'auto' ? undefined : dataspace.logo.height}
-                       style={{
-                         width: `${dataspace.logo.width}px`,
-                         height: dataspace.logo.height === 'auto' ? 'auto' : `${dataspace.logo.height}px`
-                       }}
-                     />
+             {industries.map((industry) => {
+               const IconComponent = industry.icon;
+               return (
+                 <Link 
+                   key={industry.id}
+                   to={`/Kits/industry?id=${industry.id}&ref=architecture`}
+                   className={`${styles.hexagon} ${styles[`hexagon_${industry.id.replace('-', '_')}`]}`}
+                   style={{
+                     '--hexagon-gradient': industry.gradient,
+                     '--hexagon-title-gradient': industry.gradient.replace('135deg', '45deg'),
+                   }}
+                 >
+                   <div className={styles.hexagon_content}>
+                     <div className={styles.hexagon_title}>
+                       {IconComponent && (
+                         <IconComponent 
+                           sx={{ 
+                             fontSize: 60,
+                             color: '#ffffff'
+                           }} 
+                         />
+                       )}
+                     </div>
+                     <div className={styles.hexagon_subtitle}>{industry.name}</div>
                    </div>
-                   <div className={styles.hexagon_subtitle}>{dataspace.name}</div>
-                 </div>
-               </Link>
-             ))}
+                 </Link>
+               );
+             })}
              <div className={`${styles.hexagon} ${styles.hexagon_other}`}>
                <div className={styles.hexagon_content}>
                 <div className={`${styles.hexagon_title} ${styles.other_title}`}>+</div>
-                 <div className={styles.hexagon_subtitle}>Other Dataspaces</div>
+                 <div className={styles.hexagon_subtitle}>Other Industries</div>
                </div>
              </div>
            </div>
@@ -97,7 +98,7 @@
                </div>
              </Link>
              <Link to="/Kits/dataspace-foundation" className={styles.rectangle_link}>
-               <div className={`${styles.rectangle} ${styles.rectangle_dataspace}`}>
+               <div className={`${styles.rectangle} ${styles.rectangle_industry}`}>
                  <span>Dataspace Foundation</span>
                  <div className={styles.rectangle_hover_indicator}>
                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
@@ -111,12 +112,12 @@
          
          {/* Disclaimer positioned outside and below the diagram */}
          <div className={styles.disclaimer}>
-           *All dataspace logos are trademarks, consult the respective websites for copyright information.
+           *All industry logos and trademarks are property of their respective organizations.
          </div>
          
          {/* Interactive CTA Button */}
          <div className={styles.button_container}>
-           <Link to="/documentation/kit-getting-started" className={styles.explore_button}>
+           <Link to="/documentation/kit-framework" className={styles.explore_button}>
              <span className={styles.button_text}>Click here to explore more about KITs</span>
              <svg className={styles.button_icon} width="20" height="20" viewBox="0 0 24 24" fill="none">
                <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
